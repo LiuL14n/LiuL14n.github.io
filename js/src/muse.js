e85ccfae48dc4e826dca1bbd76f6f4f2d1e16bf6
LiuL14n <1125214419@qq.com>
2020-04-20 02:23:39 +0800

    Site updated: 2020-04-20 02:23:18

--- js/src/muse.js
+++ js/src/muse.js
@@ -1,3 +1,3 @@
-// build time:Fri Apr 17 2020 19:38:45 GMT+0800 (GMT+08:00)
+// build time:Mon Apr 20 2020 02:23:16 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
