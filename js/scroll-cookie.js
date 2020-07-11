d1104675dc4acca430c315f3ce295192425b06e2
LiuL14n <1125214419@qq.com>
2020-07-12 02:34:18 +0800

    Site updated: 2020-07-12 02:33:56

--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Jul 03 2020 23:53:24 GMT+0800 (GMT+08:00)
+// build time:Sun Jul 12 2020 02:33:53 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
--- js/src/muse.js
+++ js/src/muse.js
@@ -1,3 +1,3 @@
-// build time:Fri Jul 03 2020 23:53:24 GMT+0800 (GMT+08:00)
+// build time:Sun Jul 12 2020 02:33:53 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
